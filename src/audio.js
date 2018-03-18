205814d5048e534ad4b7df5a3234a4d70b9fd37c
Deilan <deilan@mail.ru>
2018-03-19 01:31:52 +0300

    add readme

--- src/audio.js
+++
@@ -1,9 +0,0 @@
-window.analyzeAudio = function (stream) {
-    var audioContext = new(window.AudioContext || window.webkitAudioContext)();
-    var analyser = audioContext.createAnalyser();
-    source = audioCtx.createMediaStreamSource(stream);
-    source.connect(analyser);
-    var bufferLength = analyser.frequencyBinCount;
-    var dataArray = new Uint8Array(bufferLength);
-    analyser.getByteTimeDomainData(dataArray);
-}
